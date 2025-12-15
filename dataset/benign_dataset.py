dd0f5bb7c8e714edcfc78e50af08d009a5f25266
TrishaSrikanth-459 <73068694+TrishaSrikanth-459@users.noreply.github.com>
2025-12-15 12:38:17 -0500

    Implement BenignDataset class with interaction traces
    Signed-off-by: TrishaSrikanth-459 <73068694+TrishaSrikanth-459@users.noreply.github.com>

--- dataset/benign_dataset.py
+++ dataset/benign_dataset.py
@@ -1,1 +1,33 @@
+from typing import Iterable, Tuple
+from .interaction_trace import InteractionTrace
 
+class BenignDataset:
+    """
+    Immutable dataset consisting of one interaction trace per user
+    """
+
+    __slots__ = ("_traces",)
+
+    def __init__(self, traces: Iterable[InteractionTrace]):
+        self._traces: Tuple[InteractionTrace, ...] = tuple(traces)
+
+        if not self._traces:
+            raise ValueError("BenignDataset must contain at least one trace")
+
+        def __len__(self) -> int:
+            return len(self._traces)
+
+        def get_trace(self, user_index: int) -> InteractionTrace:
+            return self._traces[user_index]
+
+        def __getitem__(self, user_index: int) -> InteractionTrace:
+            return self.get_trace(user_index)
+
+        def traces(self) -> Tuple[InteractionTrace, ...]:
+            """
+            Returns the underlying immutable tuple of interaction traces
+            """
+            return self._traces
+
+        def __repr__(self) -> str:
+            return f"BenignDataset(num_users={len(self._traces)}"
