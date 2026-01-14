39c91d753832c7252c2d4a833b9972c0a4b23734
TrishaSrikanth-459 <73068694+TrishaSrikanth-459@users.noreply.github.com>
2026-01-14 09:55:45 -0500

    Create test_neighboring_dataset.py
    Signed-off-by: TrishaSrikanth-459 <73068694+TrishaSrikanth-459@users.noreply.github.com>

---
+++ tests/test_neighboring_dataset.py
@@ -0,0 +1,19 @@
+from dataset.neighboring_dataset import NeighboringDataset
+from dataset.benign_dataset import BenignDataset
+from dataset.interaction_trace import InteractionTrace
+
+def test_neighboring_dataset_replacement():
+    base = BenignDataset([
+        InteractionTrace(["a"]),
+        InteractionTrace(["b"]),
+        InteractionTrace(["c"]),
+    ])
+
+    replacement = InteractionTrace(["X"])
+
+    nd = NeighboringDataset(base, {1: replacement})
+
+    assert len(nd) == 3
+    assert nd[0] == base[0]
+    assert nd[1] == replacement
+    assert nd[2] == base[2]
