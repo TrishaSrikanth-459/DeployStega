d9ea91435289162ec81c0406dc10437c9bf5b0e9
Motunrayo Akinsete <34217098+Ecklene@users.noreply.github.com>
2026-01-19 15:34:05 +0100

    Update __init__.py
    Signed-off-by: Motunrayo Akinsete <34217098+Ecklene@users.noreply.github.com>

--- features/behaviourial/__init__.py
+++ features/behaviourial/__init__.py
@@ -1,9 +1,13 @@
 from .session import SessionFeatureExtractor
 from .timing import TimingFeatureExtractor
 from .transition import TransitionFeatureExtractor
+from .frequency import FrequencyFeatureExtractor
+from .revisit import RevisitFeatureExtractor
 
 __all__ = [
     "SessionFeatureExtractor",
     "TimingFeatureExtractor",
     "TransitionFeatureExtractor",
+    "FrequencyFeatureExtractor",
+    "RevisitFeatureExtractor",
 ]
